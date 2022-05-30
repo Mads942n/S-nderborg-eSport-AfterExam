b3f9888639c7e90e2eb1eb72546633452157b345
Mads942n <72200389+Mads942n@users.noreply.github.com>
2022-05-30 10:51:44 +0200

    Html opsætning mobil

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+/*
+To do:
+
+
+Alle sider:
+    - burger menu.
+    - Search icon bliver til Search bar.
+
+Forside:
+    - Slideshow med dots.
+
+Hold:
+    -Hele siden er et slideshow.
+    -Slideshow skal være interaktivt.
+
+Vores Team:
+    -Træner kort, info skal hentes ned fra JSON fil.
+    -Oprette player cards når siden loader med info fra JSON fil.
+    Eksempel på html opstilling af player card:
+        <article>
+            <h1 class="Navn-Stilling"></h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <img src="" alt="Spiller Billede">
+        </article>
+
+
+
+
+
+
+
+
+Google maps integration:
+https://developers.google.com/maps/documentation/javascript/adding-a-google-map#maps_add_map-javascript
+
+
+
+
+
+*/
