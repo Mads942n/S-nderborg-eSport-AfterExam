f74f9d78a754281437546fbedb73d155ee715776
EmmaKlein99 <emma009g@easv365.dk>
2022-06-02 15:52:28 +0200

    scss changes on praktisk information

--- praktiskinfo__script.js
+++ praktiskinfo__script.js
@@ -1,14 +1,2 @@
-const article__dropdown__title = document.querySelectorAll('.dropdown__title')
-const article__dropdown = document.querySelectorAll('.dropdown')
-
-
-article__dropdown__title.forEach( i => {
-    i.addEventListener('click', dropdown => {
-    i.parentNode.classList.add('dropdown--animation')
-    console.log(i.parentNode)
-
-    setTimeout(() =>{
-        i.parentNode.classList.remove('dropdown--animation')
-    }, 500);
-    })
-})
+const article__dropdown__title = document.querySelectorAll(".dropdown__button");
+const article__dropdown = document.querySelectorAll(".dropdown");
